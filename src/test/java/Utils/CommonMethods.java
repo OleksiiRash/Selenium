92d3fa7103dcdd3750b5414579ba0bf03f22f722
Alex <alexey.rashevskyi@gmail.com>
2023-07-13 11:42:43 -0400

    add the drop-down methods

--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -62,13 +62,6 @@ public class CommonMethods {
         element.sendKeys(text);
     }
 
-    //method to select from dropdown(single select)
-    public static void dropDownSelect(WebElement element, String byVisibleText) {
-        var object = new Select(element);
-        object.selectByVisibleText(byVisibleText);
-        //System.out.println("DropDown selected Element is: " + byVisibleText);
-    }
-
     // method for taking the screenshots
     public static void takeScreenshot(String name) {
         var ts = (TakesScreenshot) driver;
@@ -82,18 +75,21 @@ public class CommonMethods {
     }
 
 
-    //single checkbox method & implicitWait
-    public static void singleCheckBox(WebElement name, boolean enableImplicitWait) {
+    //single checkbox method and boolean implicitWait
+    //Example: WebElement variable = driver.findElement...
+    public static void singleCheckBox(WebElement variable, boolean enableImplicitWait) {
         if (enableImplicitWait) {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
 
-        if (!name.isSelected()) {
-            name.click();
+        if (!variable.isSelected()) {
+            variable.click();
         }
     }
 
-    //multiply checkbox selection
+    //multiply checkbox selection and option to select all checkboxes
+    //Example: List<WebElement> variable = driver.findElements...
+    //Example: List<String> attributeValuesToSelect = Arrays.asList("Option-2", "Option-4");
     public static void checkBoxMultiple(List<WebElement> variable, List<String> attributeValuesToSelect, boolean selectAllOrNot) {
         for (var select : variable) {
             var codeValue = select.getAttribute("value");
@@ -104,6 +100,29 @@ public class CommonMethods {
                 //break;
             }
         }
+    }
+
+
+    //method to select from dropdown(single select)
+    public static void dropDownSelect(WebElement element, String byVisibleText) {
+        var object = new Select(element);
+        object.selectByVisibleText(byVisibleText);
+    }
+
+    //method for multiply drop-down selection and select all options at once
+    public static void multiplyDropDown(WebElement variable, List<String> attributeValuesToSelect, boolean selectAll) {
+        var obj = new Select(variable);
+        var options = obj.getOptions();
+        for (var option : options) {
+            var optionText = option.getText();
+            if (attributeValuesToSelect.contains(optionText) || selectAll) {
+                option.click();
+                //break;
+            }
+
+        }
+
+
     }
 
 
--- src/test/java/class04/MultiDD.java
+++ src/test/java/class04/MultiDD.java
@@ -5,20 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MultiDD extends CommonMethods {
     public static void main(String[] args) throws InterruptedException {
 
         String url = "http://practice.syntaxtechs.net/basic-select-dropdown-demo.php";
         openBrowserGetURL(url, "chrome");
-        WebElement multiSelect = driver.findElement(By.cssSelector("select[name='States']"));
-        Select obj = new Select(multiSelect);
-        System.out.println("Multi Select: " + obj.isMultiple());
-        obj.selectByVisibleText("California");
-        obj.selectByValue("Ohio");
-        obj.selectByIndex(2);
-        Thread.sleep(2000);
-        // we can use as well deselect method
-        obj.deselectByIndex(2);
+
+        //var citiesDrop = driver.findElement(By.id("select-demo"));
+        //dropDownSelect(citiesDrop, "Thursday");
+
+        var newMultiSelect = driver.findElement(By.id("multi-select"));
+        List<String> attributeValuesToSelect = Arrays.asList("Ohio", "New Jersey");
+        multiplyDropDown(newMultiSelect, attributeValuesToSelect, true);
 
 
     }
